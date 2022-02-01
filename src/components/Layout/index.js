afdde80d4124559497564dc42b9509c9771d7b56
Isaiah Fowler <greatpup1940@gmail.com>
2022-02-01 16:13:00 -0500

    Removed cursor

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,25 +3,6 @@ import React, { useEffect } from "react";
 import * as styles from "../../styles/Cursor.module.css";
 
 export default function Index({ children }) {
-  useEffect(() => {
-    const cursor = document.querySelector(".cursor");
-
-    document.addEventListener("mousemove", (e) => {
-      cursor.setAttribute(
-        "style",
-        "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
-      );
-    });
-
-    document.addEventListener("click", () => {
-      cursor.classList.add("expand");
-
-      setTimeout(() => {
-        cursor.classList.remove("expand");
-      }, 500);
-    });
-  }, []);
-
   return (
     <div>
       <div className='cursor'></div>
